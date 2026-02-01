aac05a2ec1bb765c4d1b4a0e56aae65f779a4dd0
gyk419 <gyk419@gmail.com>
2026-02-01 16:43:42 +0900

    from 1 to 14 sort

---
+++ src/HelloJava.java
@@ -0,0 +1,8 @@
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("hello java");
+        // 관례상 클래스명 첫글자 대문자
+        //publick static void main(String[] args){} --> psvm 입력 후 tap or enter
+        //System.out.println --> sout 입력 후 tap or enter
+    }
+}
---
+++ src/variable/Var1.java
@@ -0,0 +1,18 @@
+package variable;
+
+public class Var1 {
+    public static void main(String[] args) {
+        // 리터럴 : 직접 입력한 고정된 값 --> (100, 10.5 ,true, A, AAAA)
+        int a = 100;
+        double b = 10.5;
+        boolean c = true;
+        char d = 'A';
+        String e = "AAAA";
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
